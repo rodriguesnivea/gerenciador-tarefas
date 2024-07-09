b901b41c91548fc325ae580392b3f44ede727a25
rodriguesnivea <rodriguesnivea779@gmail.com>
2024-07-09 14:14:32 -0300

    Adicionando novo endpoint para teste

--- src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
+++ src/main/java/com/gerenciador/tarefas/controller/TesteApiController.java
@@ -1,12 +1,18 @@
 package com.gerenciador.tarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TesteApiController {
     @GetMapping("/teste-api")
-    private String teste(){
+    public String teste(){
         return "Sucesso";
+    }
+
+    @GetMapping("/teste-bem-vindo-api")
+    public String testeBemVindo (@RequestParam(name = "nome") String nome){
+        return "Olá " +nome+ ", seja bem vindo! ";
     }
 }
